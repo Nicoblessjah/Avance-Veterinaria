bcf1a431a75a612f03b3afe0f9f74845f3bb30e5
Matias Gutierrez <168783076+mmaattyy@users.noreply.github.com>
2024-10-23 01:19:20 -0300

    feat: se añadio barra y pie de pagina de cliente

---
+++ vet_pet/src/components/PiePagina.vue
@@ -0,0 +1,34 @@
+<template>
+    <footer>
+      <p>© Veterinaria PetVida 2024</p>
+    </footer>
+  </template>
+  
+  <script>
+  </script>
+  
+  <style scoped>
+  html, body {
+      height: 100%;
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+  }
+  
+  footer {
+      position: fixed; /* Fija el footer a la parte inferior */
+      background-color: #AECBFA;
+      text-align: center;
+      bottom: 0;
+      left: 0; /* Asegura que esté alineado al borde izquierdo */
+      width: 100%; /* Se asegura de que ocupe todo el ancho */
+      height: 40px; /* Altura del footer */
+      display: flex;
+      align-items: center; /* Centra verticalmente el texto */
+      justify-content: center; /* Centra horizontalmente el texto */
+      font-size: 14px;
+      color: #333;
+      box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1); /* Opcional: agrega una sombra */
+  }
+  </style>
+  
